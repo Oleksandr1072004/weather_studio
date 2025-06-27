aca680f98f439f433f66246b18bf7e972fe2d90a
Oleksandr Grinenko <69903565+Oleksandr1072004@users.noreply.github.com>
2025-06-27 19:27:45 +0300

    Delete weather_app directory

--- weather_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from . import views
-
-app_name = "weather_app"
-
-urlpatterns = [
-    path("", views.index, name='index'),
-    path("get_city/", views.get_city, name='detail'),
-]

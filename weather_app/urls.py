e64b7257297c5f01b599bb4e32374e78b0b9ec4e
Oleksandr Grinenko <69903565+Oleksandr1072004@users.noreply.github.com>
2025-07-19 14:20:36 +0300

    Update urls.py

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -6,6 +6,4 @@ app_name = "weather_app"
 urlpatterns = [
     path("", views.index, name='index'),
     path("get_city/", views.get_city, name='detail'),
-    path("get_city_celcius/", views.get_city_celcius, name='detail_celcius'),
-    path("get_city_farenheit/", views.get_city_farenheit, name='detail_farenheit'),
 ]
